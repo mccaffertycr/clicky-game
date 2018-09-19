abd7ef82873f07b026107023170f7d7ac093658c
mccaffertycr <mccaffertycr@gmail.com>
2018-09-19 15:53:32 -0400

    style adjustment

--- src/components/imgCard/imgCard.js
+++ src/components/imgCard/imgCard.js
@@ -3,11 +3,10 @@ import './imgCard.css';
 
 const ImgCard = props => (
   <div
-      className={props.key}
-      value={props.name}
+      id={props.name}
       onClick={() => props.handleClick(props.name)}
     >
-      <img data-key={props.key} className="img-fluid img-thumbnail" alt={props.name} src={props.img_url} />
+      <img className="img-thumbnail" alt={props.name} src={props.img_url} />
   </div>
 );
 
--- src/components/scoreboard/scoreboard.js
+++ src/components/scoreboard/scoreboard.js
@@ -6,9 +6,9 @@ const Scoreboard = props => (
     <div className="row">
       <div className="col-sm-6">
         <div className="row">
-          <div className="glove1 col-sm-1"></div>
+          <div className="glove1 col-sm-1 mr-auto"></div>
           <p className="scoreboard col-sm-10">{props.msg}</p>
-          <div className="glove2 col-sm-1"></div>
+          <div className="glove2 col-sm-1 ml-auto"></div>
         </div>
       </div>
       <div className="col-sm-6">
